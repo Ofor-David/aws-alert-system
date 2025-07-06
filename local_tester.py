d692cee62ab828906e3c8e9ce4e454ba3701b4d2
Ofor David <davidoforincloud@gmail.com>
2025-07-06 21:00:32 +0100

    optimisations and code clean up

--- local_tester.py
+++
@@ -1,9 +0,0 @@
-import json
-from local_func import lambda_handler
-
-# Load the sample log
-with open('sample_log.json') as f:
-    sample_event = json.load(f)
-
-# Run the Lambda function
-lambda_handler(sample_event, None)
